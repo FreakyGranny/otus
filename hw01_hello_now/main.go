78df2050f47638b365a2a547e5498a531eadfa75
p.fomin <fomin_pw@mail.ru>
2020-05-30 23:04:42 +0700

    hw 01 solution

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	fmt.Printf("current time: %s\n", time.Now().Round(time.Second))
+
+	ntpTime, err := ntp.Time("0.ru.pool.ntp.org")
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	fmt.Printf("exact time: %s\n", ntpTime.Round(time.Second))
 }
